636845ce8f9f494601eeb95c697a4026332ab4b5
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-09-27 09:44:54 -0300

    config alias import, import assets

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,7 @@
 import { Text, View } from "react-native";
-
 export function Groups() {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Groups</Text>
     </View>
   )
