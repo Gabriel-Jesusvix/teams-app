b60faa250998712d245816c6b275b430c6b501a2
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-09-27 17:51:24 -0300

    Config fonts and create component login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { Groups } from '@screens/Groups'
 import theme from '@theme/index'
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading'
+
 export default function App () {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  })
+
+  // if (!fontsLoaded) {
+  //   return
+  // }
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {
+       !fontsLoaded ? <Groups /> : <Loading />
+      }
     </ThemeProvider>
   )
 }
