dee4ca54f12b55b3cb2e503f008a8a547e73ba03
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-10-02 23:35:50 -0300

    Add lib react navigation and create routes in the app

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import theme from '@theme/index'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading'
-// import { NewGroup } from '@screens/NewGroup'
-import { Players } from '@screens/Players'
+import { Routes } from '@routes/index'
 
 export default function App () {
   const [fontsLoaded] = useFonts({
@@ -20,7 +19,7 @@ export default function App () {
         backgroundColor="transparent"
       />
       {
-       fontsLoaded ? <Players /> : <Loading />
+       fontsLoaded ? <Routes /> : <Loading />
       }
     </ThemeProvider>
   )
